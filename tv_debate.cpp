ddcaaaa288d499046b814d9874ca1fb9b49cd0da
furkan-bulgur <bulgur.furkan27@gmail.com>
2021-06-04 01:20:51 +0300

    breaking news fundation

--- tv_debate.cpp
+++ tv_debate.cpp
@@ -34,6 +34,10 @@ time_t start_time;
 clock_t start;
 bool moderator_talks = false;
 bool commentator_talks[] = {false};
+bool show_continues = true;
+bool breaking_event_occured = false;
+bool commentator_interrupted = false;
+bool breaking_event_finished = false;
 
 
 //barrier
@@ -42,9 +46,15 @@ pthread_barrier_t ask_to_answer_barrier;
 pthread_t commentators[MAX_NUMBER_COMMENTATORS];
 pthread_cond_t comment_conds[MAX_NUMBER_COMMENTATORS];
 pthread_t moderator;
+pthread_t breaking_news_logger;
 
 pthread_cond_t ask_question;
 pthread_cond_t finish_talk;
+pthread_cond_t wait_breaking_news;
+pthread_cond_t breaking_event;
+pthread_cond_t breaking_event_finish;
+pthread_mutex_t breaking_event_mutex;
+pthread_mutex_t wait_breaking_news_mutex;
 pthread_mutex_t access_global_queue_mutex;
 pthread_mutex_t talk_mutex;
 pthread_mutex_t question_mutex;
@@ -126,9 +136,29 @@ void *request(void *com_num) {
         pthread_mutex_lock(&talk_mutex);
         pthread_cond_wait(&comment_conds[commentator_num],&talk_mutex);
         commentator_talks[commentator_num] = true;
-        pthread_sleep(speak_time);
+        double seconds = (double)speak_time;
+        struct timeval tp;
+        struct timespec timetoexpire;
+        // When to expire is an absolute time, so get the current time and add
+        // it to our delay time
+        gettimeofday(&tp, NULL);
+        long new_nsec = tp.tv_usec * 1000 + (seconds - (long)seconds) * 1e9;
+        timetoexpire.tv_sec = tp.tv_sec + (long)seconds + (new_nsec / (long)1e9);
+        timetoexpire.tv_nsec = new_nsec % (long)1e9;
+        pthread_mutex_lock(&breaking_event_mutex);
+        pthread_cond_timedwait(&breaking_event, &breaking_event_mutex, &timetoexpire);
+        commentator_interrupted = true;
         //print_time();
-        printf("Commentator #%d finishes speaking.\n",commentator_num);
+        if(breaking_event_occured){
+          printf("Commentator #%d cut short due to breaking news.\n",commentator_num);
+        }else{
+          printf("Commentator #%d finishes speaking.\n",commentator_num);
+        }
+        breaking_event_finished = false;
+        pthread_mutex_lock(&breaking_event_mutex);
+        pthread_cond_wait(&breaking_event_finish,&breaking_event_mutex);
+        breaking_event_finished = true;
+
         commentator_talks[commentator_num] = false;
         pthread_mutex_unlock(&talk_mutex);
         while(!moderator_talks){
@@ -145,7 +175,7 @@ void *request(void *com_num) {
 }
 
 void *moderate(void *vargp) {
-
+  show_continues = true;
   for(int i=0; i<num_questions; i++){
     //print_time();
     pthread_sleep(1);
@@ -176,7 +206,31 @@ void *moderate(void *vargp) {
     req_id = 0;
 
   }
+  show_continues = false;
+  pthread_exit(0);
+}
 
+void *log_breaking_news(void *vargp){
+  while(show_continues){
+    pthread_mutex_lock(&wait_breaking_news_mutex);
+    pthread_cond_wait(&wait_breaking_news,&wait_breaking_news_mutex);
+    breaking_event_occured = true;
+    printf("Breaking news!\n");
+    while(!commentator_interrupted){
+      pthread_cond_signal(&breaking_event);
+    }
+    commentator_interrupted = false;
+
+    pthread_sleep(5);
+    while(!breaking_event_finished){
+      pthread_cond_signal(&breaking_event_finish);
+    }
+    printf("Breaking news ends\n");
+
+    breaking_event_occured = false;
+    pthread_mutex_unlock(&breaking_event_mutex);
+    pthread_mutex_unlock(&wait_breaking_news_mutex);
+  }
   pthread_exit(0);
 }
 
@@ -221,6 +275,11 @@ bool initialize_threads(){
   pthread_mutex_init(&talk_mutex, NULL);
   pthread_mutex_init(&access_global_queue_mutex, NULL);
   pthread_mutex_init(&question_mutex, NULL);
+  pthread_mutex_init(&wait_breaking_news_mutex, NULL);
+  pthread_mutex_init(&breaking_event_mutex, NULL);
+  pthread_cond_init(&breaking_event,NULL);
+  pthread_cond_init(&breaking_event_finish,NULL);
+  pthread_cond_init(&wait_breaking_news,NULL);
   pthread_cond_init(&ask_question,NULL);
   pthread_cond_init(&finish_talk,NULL);
 
@@ -235,6 +294,8 @@ bool initialize_threads(){
   }
 
   if(pthread_create(&moderator, &thread_attribute, moderate, NULL) != 0)
+    return false;
+  if(pthread_create(&breaking_news_logger, &thread_attribute, log_breaking_news, NULL) != 0)
     return false;
 
   return true;
@@ -251,12 +312,28 @@ int main(int argc, char *argv[]) {
     printf("Thread Error. Exiting.\n");
   }
 
+
+
+  while(show_continues){
+    if(!breaking_event_occured){
+      pthread_sleep(1);
+      if((rand()/(float)RAND_MAX) < prob_to_answer) {
+        pthread_cond_signal(&wait_breaking_news);
+      }
+    }
+  }
+
+
+
+
+
   // join created threads.
   for(long i=0; i<num_commentators; i++){
     pthread_join(commentators[i], NULL);
     pthread_cond_destroy(&comment_conds[i]);
   }
 
+  pthread_join(breaking_news_logger, NULL);
   pthread_join(moderator, NULL);
 
   // destroy attr and mutex.
@@ -265,6 +342,11 @@ int main(int argc, char *argv[]) {
   pthread_mutex_destroy(&access_global_queue_mutex);
   pthread_mutex_destroy(&talk_mutex);
   pthread_mutex_destroy(&question_mutex);
+  pthread_mutex_destroy(&breaking_event_mutex);
+  pthread_mutex_destroy(&wait_breaking_news_mutex);
+  pthread_cond_destroy(&wait_breaking_news);
+  pthread_cond_destroy(&breaking_event_finish);
+  pthread_cond_destroy(&breaking_event);
   pthread_cond_destroy(&ask_question);
   pthread_cond_destroy(&finish_talk);
   pthread_exit(NULL);
